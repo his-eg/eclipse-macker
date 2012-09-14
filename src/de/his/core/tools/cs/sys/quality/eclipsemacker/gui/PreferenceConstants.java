45a093bab111d6b754896ba8203cbb0ed974a99d
Markus Keunecke <markuskeunecke@users.sourceforge.net>
2012-09-14 15:57:09 +0200

    java doc

--- src/de/his/core/tools/cs/sys/quality/eclipsemacker/gui/PreferenceConstants.java
+++ src/de/his/core/tools/cs/sys/quality/eclipsemacker/gui/PreferenceConstants.java
@@ -13,13 +13,42 @@ public class PreferenceConstants {
     
 	/** Macker Events */
     public static final String WARNING = "WARNING";
+
+    /**
+     * Error Event 
+     */
     public static final String ERROR = "ERROR";
+
+    /**
+     * Default Event given from macker
+     */
     public static final String DEFAULT = "DEFAULT";
+
+    /**
+     * Classpath filter
+     */
     public static final String CLASSPATH_FILTER = "FILTER";
+
+    /**
+     * use global settings
+     */
     public static final String USE_HIS_SETTINGS = "USE_HIS_SETTINGS";
+
+    /**
+     * use classpath filter
+     */
     public static final String USE_CLASSPATH_FILTER = "USE_FILTER";
+
+    /**
+     * source filter
+     */
     public static final String SOURCE_FILTER = "S_FILTER";
+
+    /**
+     * use source filter
+     */
     public static final String USE_SOURCE_FILTER = "USE_S_FILTER";
+
     /**Severity Choice*/
     public static final String CHOICE = "CHOICE";
 
